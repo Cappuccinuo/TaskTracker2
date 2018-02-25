9cf798a899249476ff550475d80f8380f57532f4
Yuan Gao <gaoyuanbj@yahoo.com>
2018-02-25 18:32:47 -0500

    Finish the date format part

--- tasktracker/lib/tasktracker_web/views/page_view.ex
+++ tasktracker/lib/tasktracker_web/views/page_view.ex
@@ -3,4 +3,15 @@ defmodule TasktrackerWeb.PageView do
 
   def complete_status(true), do: "Finished"
   def complete_status(false), do: "Working"
+
+  def cast_time(time) do
+    dt = DateTime.truncate(time, :second)
+    %DateTime{year: y, month: m, day: d} = dt
+    if (y == 1970 and m == 1 and d == 1) do
+      "Not end yet"
+    else
+      {:ok, time} = Ecto.DateTime.cast(dt)
+      time
+    end
+  end
 end
